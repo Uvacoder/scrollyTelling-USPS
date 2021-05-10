6549c0334285ae40c2fc3e9d3a6c3851ea1d87ba
Matt Ashford <mattashford1@gmail.com>
2021-05-10 10:01:17 -0400

    restructure again

--- scrollFunctions.js
+++ scrollFunctions.js
@@ -6,22 +6,6 @@ import { per } from "./data/dataPrep.js";
 
 console.log("tot", totalMdVolume);
 console.log("per", per);
-// console.log(cow);
-// console.log(cra);
-
-// const mdTemp = cra.filter((row) => row.class == "MD");
-// let totalMd = [];
-
-// function rowConverter(row) {
-//   return {
-//     date: row.fy,
-//     value: row.vol,
-//   };
-// }
-
-// mdTemp.forEach((row) => {
-//   totalMd.push(rowConverter(row));
-// });
 
 const changeEvents = {
   lineGraph: 0,
@@ -66,3 +50,40 @@ console.log(
     return d.value;
   })
 );
+
+// let main = d3.select("main");
+// var scrolly = main.select("#scrolly");
+// var article = scrolly.select("article");
+// var step = article.selectAll(".step");
+
+// scroller
+//   .setup({
+//     step: "#scrolly article .step",
+//     offset: 0.33,
+//     debug: true,
+//   })
+//   .onStepEnter(handleStepEnter);
+
+// function handleStepEnter(resp) {
+//   const scrollPoz = window.scrollY;
+
+//   bar(totalMdVolume);
+
+//   //   if (scrollPoz <= changeEvents.barGraph) {
+//   //     updateLine(filteredData);
+//   //     //   bar(rawDataGroup);
+//   //   } else {
+//   //     bar(rawDataGroup);
+//   //     //   // update(filteredData);
+//   //   }
+// }
+
+// bar(totalMdVolume);
+
+// // there is hope!!
+
+// console.log(
+//   d3.max(totalMdVolume, (d) => {
+//     return d.value;
+//   })
+// );
